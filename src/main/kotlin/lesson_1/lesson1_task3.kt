2e55200cf6d67044410fb44725fac681afe0dc6c
Nikita Popov <nikita.popovv@list.ru>
2024-11-21 22:05:55 +0300

    KS-1-3 Task 3 Complete

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main(){
+    val year: Short = 1961
+    var hour: Short = 9
+    var minute: Short = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(':')
+    print(minute)
+}
