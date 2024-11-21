1df955a5de4950d1d876e4363325168dcf7f9a9f
Nikita Popov <nikita.popovv@list.ru>
2024-11-22 00:26:36 +0300

    KS-2-1 Task 1 Complete

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main(){
+    val mark1 = 3.0
+    val mark2 = 4.0
+    val mark3 = 3.0
+    val mark4 = 5.0
+    val avgMark = (mark1 + mark2 + mark3 + mark4) / 4
+    println(avgMark)
+}
