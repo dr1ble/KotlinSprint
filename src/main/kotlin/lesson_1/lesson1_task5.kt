41d93a1f7b6cf8886061b1b4b7fa3bc23ec7d4df
Nikita Popov <nikita.popovv@list.ru>
2024-11-21 22:53:53 +0300

    KS-1-5 Task 5 Complete

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_1
+
+fun main(){
+    val totalSeconds = 6480
+    val totalHours = totalSeconds / 60 * 60
+    val totalMinutes = totalSeconds % 3600 / 60
+    val lastSeconds = totalSeconds % 60
+    println(String.format("%02d:%02d:%02d", totalHours, totalMinutes, lastSeconds))
+}
