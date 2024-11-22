ec6d1313cd9d5719b2ea794c63091bb08b6d0d05
Nikita Popov <nikita.popovv@list.ru>
2024-11-23 00:15:02 +0300

    KS-2-2 Task 2 Complete

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_2
+
+fun main() {
+    val workersQuantity = 50
+    val workerSalary = 30000
+    val traineeQuantity = 30
+    val traineeSalary = 20000
+
+    val regularWorkersSalaries = workersQuantity * workerSalary
+    val totalSpendsOnSalaries = regularWorkersSalaries + traineeQuantity * traineeSalary
+    val avgSalariyOnPerson = totalSpendsOnSalaries / workersQuantity + traineeQuantity
+
+    println(regularWorkersSalaries)
+    println(totalSpendsOnSalaries)
+    println(avgSalariyOnPerson)
+}
