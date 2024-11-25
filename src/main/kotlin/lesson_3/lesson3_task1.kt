5100732408c052c40ac98f6669fbc91b04a1a7dc
Nikita Popov <nikita.popovv@list.ru>
2024-11-25 22:57:27 +0300

    KS-3-1: Приветствие в зависимости от времени суток

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting = "Good morning!"
+    val name = "Ivan"
+
+    println("$name, $greeting")
+
+    greeting = "Good evening!"
+
+    println("$name, $greeting")
+}
