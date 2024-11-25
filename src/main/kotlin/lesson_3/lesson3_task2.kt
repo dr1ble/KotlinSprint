b47dd5bbde02e2ea0cbe6af3bac546e703111e01
Nikita Popov <nikita.popovv@list.ru>
2024-11-25 23:02:50 +0300

    KS-3-2: Измение фио после свадьбы

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var secondName = "Андреева"
+    val name = "Татьяна"
+    val thirdName = "Сергеевна"
+    var age = 20
+
+    println("$secondName $name $thirdName, $age")
+
+    secondName = "Сидорова"
+    age = 22
+
+    println("$secondName $name $thirdName, $age")
+}
