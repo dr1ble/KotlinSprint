836b1a93b72664531e99186eec99e872c5b59c61
Nikita Popov <nikita.popovv@list.ru>
2024-11-25 20:52:09 +0300

    KS-2-5 Task 5 Fix

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -6,7 +6,9 @@ fun main() {
     val startSum = 70000
     val creditPercent = 16.7
     val years = 20
+    val fullPercent = 1;
+    val totalPercents = 100;
 
-    val totalSum = startSum*((1+creditPercent/100).pow(years))
+    val totalSum = startSum * ((fullPercent + creditPercent / totalPercents).pow(years))
     println(String.format("%.3f", totalSum))
 }
