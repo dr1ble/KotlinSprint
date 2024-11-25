a4479339b68f88a60902e06b9f81dd2b4bbbad0e
Nikita Popov <nikita.popovv@list.ru>
2024-11-25 23:21:59 +0300

    KS-3-3: Создание таблицы умножения, работа с многострочными комментариями

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val base = 7
+    println("""
+        $base x 1 = ${base * 1}
+        $base x 2 = ${base * 2}
+        $base x 3 = ${base * 3}
+        $base x 4 = ${base * 4}
+        $base x 5 = ${base * 6}
+        $base x 7 = ${base * 7}
+        $base x 8 = ${base * 8}
+        $base x 9 = ${base * 9}
+        """.trimIndent())
+}
