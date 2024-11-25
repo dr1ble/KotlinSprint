35c9dcf046169b913991da5359a7d807b68d586f
Nikita Popov <nikita.popovv@list.ru>
2024-11-25 20:57:29 +0300

    KS-2-1 Исправление (количество студентов записано в переменную)

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,6 +5,7 @@ fun main() {
     val mark2 = 4.0
     val mark3 = 3.0
     val mark4 = 5.0
-    val avgMark = (mark1 + mark2 + mark3 + mark4) / 4
+    val totalStudents = 4
+    val avgMark = (mark1 + mark2 + mark3 + mark4) / totalStudents
     println(avgMark)
 }
